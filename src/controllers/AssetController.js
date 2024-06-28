82c00bf110d0c5cb56ab529cdb7525c098a967a5
vinhtruong650 <chivinh2302tvh@gmail.com>
2024-06-28 14:41:40 +0700

    Vinh: add API upload image

--- src/controllers/AssetController.js
+++ src/controllers/AssetController.js
@@ -5,7 +5,8 @@ let upload = async (req, res) => {
     if(!req.file)  {
         return sendErrorResponse(res,'Upload failed: No file uploaded.',400);
     }
-    const filePath = req.file.path.replace('src/public', 'static');
+    const formatPath = req.file.path.replace(/\\/g, '/');
+    const filePath = formatPath.replace('src/public', 'static');
     return sendUploadSuccess(res,{url: `${process.env.DOMAIN}${filePath}`});
 }
 
